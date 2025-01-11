c9628568f1254b6ee5e8c5c9ac52aaf97707f537
S. MohammadMahdi Zamanian <dev@mm25zamanian.ir>
2025-01-11 17:42:46 +0330

    feat(app): implement category page
    This commit implements the category page which displays a list of items within a given category.
    
    The page dynamically generates static parameters for each category based on the data in the `db/index.json` file. It also includes a revalidation strategy to update the page content periodically.

---
+++ src/app/[category]/page.tsx
@@ -0,0 +1,50 @@
+import ItemCard from "@/components/item-card";
+import IDataBase from "@/types/db";
+import Link from "next/link";
+import _database from "~/db/index.json";
+
+export const revalidate = 60;
+
+export async function generateStaticParams() {
+  return Object.keys((_database as IDataBase).data).map((category) => ({
+    category,
+  }));
+}
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ category: string }>;
+}) {
+  const database = _database as IDataBase;
+  const category: keyof IDataBase["data"] = (await params).category;
+  const items = database.data[category].items;
+
+  return (
+    <div className="flex flex-col overflow-y-auto">
+      <div className="flex flex-wrap rounded-b-box z-10 inset-x-0 top-16 p-2 gap-2 bg-base-300">
+        {(Object.keys(database.data) as Array<keyof IDataBase["data"]>).map(
+          (__category) => (
+            <Link
+              key={__category}
+              href={`/${__category}`}
+              className="btn btn-sm flex-1 min-w-max btn-secondary data-[active='false']:btn-outline"
+              data-active={category === __category}
+              prefetch
+              replace
+            >
+              {database.data[__category].name}
+            </Link>
+          )
+        )}
+      </div>
+
+      <div className="flex flex-col gap-4 p-4">
+        {items.length > 0 &&
+          items.map((item, index) => (
+            <ItemCard item={item} key={index + item.name} />
+          ))}
+      </div>
+    </div>
+  );
+}
