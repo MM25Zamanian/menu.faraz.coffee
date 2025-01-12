2702d619d8f40f610ecceed710ee82f23c151675
S. MohammadMahdi Zamanian <dev@mm25zamanian.ir>
2025-01-12 14:22:48 +0330

    feat(app): implement landing page
    This commit implements the landing page for the application, including:
    
    - Displaying the project name and description from `next.config`.
    - Adding a button to view the menu, linking to `/coffee-based-hot`.
    - Adding a disabled button for the "Where are we?" functionality, which will be implemented later.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,26 @@
+import { projectConfig } from "#/next.config";
+import { Icon } from "@iconify/react";
+import Link from "next/link";
+
 export default function Home() {
-  return <h1>Hello</h1>;
+  return (
+    <div className="hero my-auto">
+      <div className="hero-content flex-col text-center">
+        <h1 className="text-5xl font-bold">{projectConfig.name}</h1>
+        <p className="py-3 text-balance text-center">
+          {projectConfig.description}
+        </p>
+        <div className="flex w-full gap-2">
+          <Link href="/coffee-based-hot/" className="btn btn-primary flex-1">
+            مشاهده منوی ما
+            <Icon icon="solar:documents-bold-duotone" className="size-6" />
+          </Link>
+          <Link href="/" className="btn btn-secondary btn-disabled">
+            ما کجاییم؟
+            <Icon icon="solar:point-on-map-bold-duotone" className="size-6" />
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
